aecdf8ffc6a821cb0f1b495500521e5d5d092829
tadazly <tadazly@163.com>
2024-05-09 18:26:26 +0800

    Modify Example

--- examples/triangle/mod.rs
+++ examples/triangle/mod.rs
@@ -29,6 +29,72 @@ impl App {
                 self.window.as_ref().unwrap().request_redraw();
                 true
             }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(KeyCode::Space),
+                        state: ElementState::Pressed,
+                        ..
+                    },
+                ..
+            } => {
+                let state = self.state.as_mut().unwrap();
+                let device = &state.device;
+                let config = &state.config;
+
+                let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+                    label: Some("Shader When Press Space"),
+                    source: wgpu::ShaderSource::Wgsl(include_str!("shader_press_space.wgsl").into()),
+                });
+
+                let render_pipeline_layout =
+                    device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                        label: Some("Render Pipeline Layout"),
+                        bind_group_layouts: &[],
+                        push_constant_ranges: &[],
+                    });
+
+                state.render_pipeline =
+                    device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                        label: Some("Render Pipeline"),
+                        layout: Some(&render_pipeline_layout),
+                        vertex: wgpu::VertexState {
+                            module: &shader,
+                            entry_point: "vs_main",
+                            buffers: &[],
+                            compilation_options: wgpu::PipelineCompilationOptions::default(),
+                        },
+                        fragment: Some(wgpu::FragmentState {
+                            module: &shader,
+                            entry_point: "fs_main",
+                            targets: &[Some(wgpu::ColorTargetState {
+                                format: config.format,
+                                blend: Some(wgpu::BlendState::REPLACE),
+                                write_mask: wgpu::ColorWrites::ALL,
+                            })],
+                            compilation_options: wgpu::PipelineCompilationOptions::default(),
+                        }),
+                        primitive: wgpu::PrimitiveState {
+                            topology: wgpu::PrimitiveTopology::TriangleList,
+                            strip_index_format: None,
+                            front_face: wgpu::FrontFace::Ccw,
+                            cull_mode: Some(wgpu::Face::Back),
+                            polygon_mode: wgpu::PolygonMode::Fill,
+                            unclipped_depth: false,
+                            conservative: false,
+                        },
+                        depth_stencil: None,
+                        multisample: wgpu::MultisampleState {
+                            count: 1,
+                            mask: !0,
+                            alpha_to_coverage_enabled: false,
+                        },
+                        multiview: None,
+                    });
+                
+                self.window.as_ref().unwrap().request_redraw();
+                true
+            }
             _ => false,
         }
     }
@@ -98,6 +164,8 @@ impl State {
 
         let modes = &surface_caps.present_modes;
         println!("Available present_modes: {modes:?}");
+        println!("Available surface_formats: {:?}", &surface_caps.formats);
+        println!("Current surface_format: {:?}", surface_format);
 
         let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
